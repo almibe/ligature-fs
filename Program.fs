fdc5c5ba97e39465f0ac6a5db9a078827963ba51
Alex Michael Berry <alexmiberry@gmail.com>
2022-12-23 11:27:33 -0500

    initial code and project setup, closes #1

---
+++ Program.fs
@@ -0,0 +1,34 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+    choose [
+        route "/datasets"   >=> text "\n"
+        route "/"       >=> text "root" ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ligature-fs
 Experimental F# based implementation of Ligature.
+
+## Use
+This project assumes you have .NET Core setup.
+Currently to run the application simply run
+
+`dotnet run`
+
+in the root directory of this project.
