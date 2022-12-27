d3220c6ea9053c047396b05717ade300ae5e9860
Alex Michael Berry <alexmiberry@gmail.com>
2022-12-26 22:43:44 -0500

    adding giraffe's viewengine, closes #2

---
+++ src/LigatureLab/Program.fs
@@ -0,0 +1,45 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Giraffe.ViewEngine
+
+let datasetsPage = html [] [
+        head [] [
+            title [] [ str "Ligature Lab" ]
+        ]
+        body [] [
+            h1 [] [ str "Ligature Lab" ]
+            p [ _class "some-css-class"; _id "someId" ] [
+                str "Datasets:"
+            ]
+        ]
+    ]
+
+let webApp =
+    choose [
+        route "/" >=> htmlView datasetsPage ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
