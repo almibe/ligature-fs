6be33bb26b045b76e685b179fd600cbd4daf8d52
Alex Michael Berry <alexmiberry@gmail.com>
2023-01-02 00:15:04 -0500

    starting to add Ligature Desktop, #14

---
+++ src/LigatureDesktop/MainWindow.axaml.cs
@@ -0,0 +1,39 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using Avalonia.Logging;
+
+namespace LigatureDesktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void AddDatasetClick(object sender, RoutedEventArgs e)
+    {
+	    //do something on click
+    }
+
+    private void RemoveDatasetClick(object sender, RoutedEventArgs e)
+    {
+	    //do something on click
+    }
+
+    private void RefreshDatasetClick(object sender, RoutedEventArgs e)
+    {
+	    //do something on click
+    }
+
+    private void RunClick(object sender, RoutedEventArgs e)
+    {
+	    //do something on click
+    }
+
+    private void ClearClick(object sender, RoutedEventArgs e)
+    {
+	    //do something on click
+    }
+}
