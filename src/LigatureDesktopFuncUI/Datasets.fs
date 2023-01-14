6039b0c334c141a719a0468029c6159037d5b922
Alex Michael Berry <alexmiberry@gmail.com>
2023-01-14 02:11:10 -0500

    finished rewrite of LigatureDesktop to use F#, closes #20

--- src/LigatureDesktopFuncUI/Datasets.fs
+++ src/LigatureDesktopFuncUI/Datasets.fs
@@ -5,14 +5,39 @@
 namespace LigatureDesktop
 
 module Datasets =
+    open Ligature
     open Avalonia.FuncUI
     open Avalonia.Controls
     open Avalonia.FuncUI.DSL
     open Avalonia.Layout
     
+    type UIModel = {
+        Ligature: Ligature
+        Datasets: string list
+        SelectedDataset: string
+        Output: string
+        NewDataset: string
+    }
+
+    let createModel () = { 
+        Datasets = []
+        Output = ""
+        Ligature = new LigatureInMemory.LigatureInMemory() :> Ligature
+        NewDataset = ""
+        SelectedDataset = ""
+        }
+
     let view =
-        Component.create("Datasets",fun ctx ->
-            let state = ctx.useState 0
+        Component.create("Datasets",fun (ctx: IComponentContext) ->
+            let state = ctx.useState (createModel ())
+
+            let addDataset () =
+                let datasetName = state.Current.NewDataset
+                let x = datasetName :: state.Current.Datasets
+                printfn "%A" x
+                state.Set { state.Current with Datasets = datasetName :: state.Current.Datasets }
+                printfn "test"
+
             DockPanel.create [
                 DockPanel.children [
                     StackPanel.create [
@@ -27,20 +52,49 @@ module Datasets =
                             ComboBox.create [
                                 ComboBox.verticalAlignment VerticalAlignment.Center
                                 ComboBox.margin (5, 0, 10, 0)
-                                ComboBox.dataItems [ "No Datasets" ]
+                                ComboBox.dataItems state.Current.Datasets //[ "No Datasets" ]
                                 ComboBox.selectedIndex 0
                             ]
                             Button.create [
                                 Button.verticalAlignment VerticalAlignment.Center
                                 Button.margin (10, 0, 10, 0)
                                 Button.content "Add Dataset"
-                                //Button.click call RefreshDataset
+                                Button.flyout (
+                                    Flyout.create [
+                                        Flyout.placement FlyoutPlacementMode.Bottom
+                                        Flyout.content (
+                                            StackPanel.create [
+                                                StackPanel.children [
+                                                    TextBlock.create [
+                                                        TextBlock.text "Dataset Name:"
+                                                    ]
+                                                    TextBox.create [
+                                                        TextBox.onTextChanged (fun newDataset -> state.Set { state.Current with NewDataset = newDataset })
+                                                    ]
+                                                    Button.create [
+                                                        Button.content "Add"
+                                                        Button.onClick (fun x -> addDataset ())
+                                                    ]
+                                                ]
+                                            ]
+                                        )
+                                    ]
+                                )
                             ]
                             Button.create [
                                 Button.verticalAlignment VerticalAlignment.Center
                                 Button.margin (10, 0, 10, 0)
                                 Button.content "Remove Dataset"
-                                //Button.click call RefreshDataset
+                                Button.flyout (
+                                    Flyout.create [
+                                        Flyout.placement FlyoutPlacementMode.Bottom
+                                        Flyout.content (
+                                            TextBlock.create [
+                                                TextBlock.text "Hi, I am flyout"
+                                            ]
+                                        )
+                                    ]
+                                )
                             ]
                             Button.create [
                                 Button.verticalAlignment VerticalAlignment.Center
@@ -50,22 +104,60 @@ module Datasets =
                             ]
                         ]
                     ]
-                    //
-                    // <StackPanel Orientation="Horizontal" DockPanel.Dock="Bottom">
-                    //     <TextBlock VerticalAlignment="Center" Margin="10,0,0,0">Action:</TextBlock>
-                    //     <ComboBox VerticalAlignment="Center" Margin="5,0,10,0" SelectedIndex="0">
-                    //         <ComboBoxItem>Query</ComboBoxItem>
-                    //         <ComboBoxItem>Add Statements</ComboBoxItem>
-                    //         <ComboBoxItem>Remove Statements</ComboBoxItem>
-                    //     </ComboBox>
-                    //     <Button VerticalAlignment="Center" Margin="10,0,10,0" Click="RunClick">Run</Button>
-                    //     <Button VerticalAlignment="Center" Margin="10,0,10,0" Click="ClearClick">Clear</Button>
-                    // </StackPanel>
-                    // <Grid RowDefinitions="*, 1, *">
-                    //     <TextBox Grid.Row="0" AcceptsReturn="True" TextWrapping="Wrap" Classes="code"></TextBox>
-                    //     <GridSplitter Grid.Row="1" Background="Gray" ResizeDirection="Rows"/>
-                    //     <TextBox Grid.Row="2" AcceptsReturn="True" TextWrapping="Wrap" Classes="code" IsReadOnly="true"></TextBox>
-                    // </Grid>
+                    StackPanel.create [
+                        StackPanel.orientation Orientation.Horizontal
+                        DockPanel.dock Dock.Bottom
+                        StackPanel.children [
+                            TextBlock.create [
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                                TextBlock.margin (10, 0, 0, 0)
+                                TextBlock.text "Action:"
+                            ]
+                            ComboBox.create [
+                                ComboBox.verticalAlignment VerticalAlignment.Center
+                                ComboBox.margin (5, 0, 10, 0)
+                                ComboBox.dataItems [
+                                    "Query"
+                                    "Add Statements"
+                                    "Remove Statements"
+                                ]
+                                ComboBox.selectedIndex 0
+                            ]
+                            Button.create [
+                                Button.verticalAlignment VerticalAlignment.Center
+                                Button.margin (10, 0, 10, 0)
+                                Button.content "Run"
+                                //Button.onClick (fun _ -> refreshDatasets ())
+                            ]
+                            Button.create [
+                                Button.verticalAlignment VerticalAlignment.Center
+                                Button.margin (10, 0, 10, 0)
+                                Button.content "Clear"
+                                Button.onClick (fun _ -> state.Set { state.Current with Output = "" })
+                            ]
+                        ]
+                    ]
+                    Grid.create [
+                        Grid.rowDefinitions "*, 1, *"
+                        Grid.children [
+                            TextBox.create [
+                                Grid.row 0
+                                TextBox.acceptsReturn true
+                                TextBox.classes [ "code" ]
+                            ]
+                            GridSplitter.create [
+                                Grid.row 1
+                                GridSplitter.background "Gray"
+                            ]
+                            TextBox.create [
+                                Grid.row 2
+                                TextBox.acceptsReturn true
+                                TextBox.classes [ "code" ]
+                                TextBox.isReadOnly true
+                                TextBox.text state.Current.Output
+                            ]
+                        ]
+                    ]
                 ]
             ]
         )
