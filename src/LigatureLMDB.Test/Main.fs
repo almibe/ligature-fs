badd3cb8d368bf256713f970cb65db6f53133fe2
Alex Michael Berry <alexmiberry@gmail.com>
2023-02-24 01:32:19 -0500

    initial project setup for LigatureLMDB, #33

---
+++ src/LigatureLMDB.Test/Main.fs
@@ -0,0 +1,19 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+module Ligature.LMDB.Test
+
+open Ligature.LMDB.Main
+open Expecto
+open Ligature.TestSuite
+
+[<Tests>]
+let tests = ligatureTestSuite (fun () ->
+    let instance = new LigatureLMDB()
+    instance.initialize () |> ignore
+    instance)
+
+[<EntryPoint>]
+let main argv =
+    runTestsInAssembly defaultConfig argv
