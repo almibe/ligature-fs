b3802b108c012725bc394b41f8d30bcefdaffb98
Alex Michael Berry <alexmiberry@gmail.com>
2023-03-15 01:01:34 -0400

    updating build to work with new LigatureLab setup, #54

--- src/build/build.fs
+++ src/build/build.fs
@@ -2,24 +2,25 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use execContext = Fake.Core.Context.FakeExecutionContext.Create false "path/to/script.fsx" []
+let execContext = Fake.Core.Context.FakeExecutionContext.Create false "path/to/script.fsx" []
 Fake.Core.Context.setExecutionContext (Fake.Core.Context.RuntimeContext.Fake execContext)
 
 open Fake
 open Fake.Core
 open Fake.JavaScript
+open Fake.DotNet
 
 Target.create "RunLigatureLab" (fun _ ->
     Npm.install (fun o ->
         { o with
-            WorkingDirectory = "./src/LigatureLabJS"
+            WorkingDirectory = "./src/LigatureLab/node"
         })
     Npm.run "build" (fun o ->
         { o with
-            WorkingDirectory = "./src/LigatureLabJS"
+            WorkingDirectory = "./src/LigatureLab/node"
         }
     )
-    System.IO.File.Copy("./src/LigatureLabJS/public/bundle.js", "./src/LigatureLab/WebRoot/bundle.js")
+    DotNet.exec (fun o -> o) "run" "--project ./src/LigatureLab/LigatureLab.fsproj" |> ignore
 )
 
 Target.runOrDefault "RunLigatureLab"
