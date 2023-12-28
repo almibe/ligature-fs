12fcd1f4c0e9ae8eeaa4d4cbb7dc150335847efa
Alex Michael Berry <alexmiberry@gmail.com>
2023-12-28 04:36:12 -0500

    removing ligature code, #105

--- src/LigatureFable/demo/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { readScriptResult } from "../../npmProject/Ligature.js";
-
-let res = readScriptResult("test");
-console.log(res);
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<div>${res}</div>`;
