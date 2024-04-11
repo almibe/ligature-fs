66d5478d00d9ff61a95e270e7fe9643c106b54fc
Alex Michael Berry <alexmiberry@gmail.com>
2024-04-11 00:09:20 -0400

    getting basic scripts running in LigaturePad rewrite

--- src/LigaturePad/MainWindow.axaml.cs
+++ src/LigaturePad/MainWindow.axaml.cs
@@ -1,8 +1,16 @@
-using Avalonia.Controls;
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 namespace LigaturePad;
 
+extern alias Bend;
+using _ = Bend::Ligature.Bend.Main;
+
 using Avalonia.Interactivity;
+using Avalonia.Controls;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 
 public partial class MainWindow : Window
 {
@@ -13,11 +21,9 @@ public partial class MainWindow : Window
 
     public void RunAction(object sender, RoutedEventArgs args)
     {
-        // let editorText = state.Current.EditorText
-        // let res = printResult (runScript editorText)
-        // state.Set({ ResultText = res; EditorText = editorText})) //state.Current.EditorText }))
-
-        source.Text = "Source: None";
+        //TODO use instance prelude
+        var res = _.printResult(_.run(ScriptText.Text, Bend.Ligature.Bend.Lib.Preludes.standardPrelude()));
+        ResultText.Text = res;
     }
 
     public void IntrospectAction(object sender, RoutedEventArgs args)
@@ -58,9 +64,9 @@ public partial class MainWindow : Window
     }
     public void ExitAction(object sender, RoutedEventArgs args)
     {
-        // let editorText = state.Current.EditorText
-        // let res = string (introspect editorText)
-        // state.Set({ ResultText = res; EditorText = editorText})) //state.Current.EditorText }))
-        source.Text = "Source: None";
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopApp)
+        {
+            desktopApp.Shutdown();
+        }
     }
 }
