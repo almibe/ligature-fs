37f162c8a903d1283794aeabc7d53b2364d344f4
Alex Michael Berry <alexmiberry@gmail.com>
2024-12-03 20:35:00 -0500

    clean up

--- src/LigatureZeroMQ/Program.fs
+++ src/LigatureZeroMQ/Program.fs
@@ -4,7 +4,6 @@
 
 module Ligature.ZMQ.Main
 
-open Ligature.ZMQ.Config
 open Ligature
 open Wander.Main
 open NetMQ.Sockets
@@ -27,7 +26,6 @@ let rec serve (server: ResponseSocket) =
 [<EntryPoint>]
 let main args =
     Console.WriteLine("Starting Ligature ZeroMQ.")
-    let config = readConfig ()
     use server = new ResponseSocket()
     server.Bind("tcp://localhost:4201")
     Console.WriteLine("Started on port 4201.")
