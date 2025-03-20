215f1003d3dc751bcad7e45b5433cf2a58c7fea3
Alex Michael Berry <alexmiberry@gmail.com>
2025-03-20 03:30:54 -0400

    getting basic defns working

--- src/Wander/Interpreter.fs
+++ src/Wander/Interpreter.fs
@@ -12,17 +12,17 @@ open Parser
 let rec evalScript (actions: Fns) (bindings: Bindings) (script: Script) : Result<Any, LigatureError> =
     match script with
     | [] -> Ok(Any.Network Set.empty)
-    | [ head ] -> executeExpression actions bindings head
+    | head :: [] -> executeExpression actions bindings head
     | head :: tail ->
-        // match head with
-        // | _ -> failwith "TODO"
-        // | Expression.Defn(name, lambda) ->
-        //     let bindings = Map.add name lambda bindings
-        //     evalScript actions bindings tail
-        // | Expression.Application application ->
-        match executeExpression actions bindings head with
-        | Ok _ -> evalScript actions bindings tail
-        | Error err -> Error err
+        match head with
+        | Any.Application(Term "defn", [ Any.Term name; Any.Quote args; value ]) ->
+            //TODO process args
+            let bindings = Map.add name ([], value) bindings
+            evalScript actions bindings tail
+        | _ ->
+            match executeExpression actions bindings head with
+            | Ok _ -> evalScript actions bindings tail
+            | Error err -> Error err
 
 and createFn (doc: string) (script: Script) examples pre post : Fn =
     Fn(
@@ -65,10 +65,10 @@ and evalRecord (actions: Fns) (bindings: Bindings) (record: Record) : Record =
             | other -> other)
         record
 
-and evalLambda (fns: Fns) (bindings: Bindings) (lambda: Lambda) : Result<Any, LigatureError> = failwith "TODO"
-//     let args, body = lambda
-//     //TODO bind args
-//     evalScript fns bindings [Expression.Application [body]]
+and evalLambda (fns: Fns) (bindings: Bindings) (lambda: Lambda) : Result<Any, LigatureError> =
+    let args, body = lambda
+    //TODO bind args
+    executeExpression fns bindings body
 
 and executeApplication (actions: Fns) (bindings: Bindings) (application: Application) : Result<Any, LigatureError> =
     // let application = rewriteApplication expression
@@ -91,6 +91,26 @@ and executeApplication (actions: Fns) (bindings: Bindings) (application: Applica
                     | _ -> value)
                 args)
     | None, None -> error $"Could not find function {fn}" None
+// match bindings.TryFind fn, actions.TryFind fn with
+// | Some lambda, _ -> evalLambda actions bindings lambda
+// | None, Some(Fn(_, fn)) ->
+//     fn
+//         actions
+//         bindings
+//         (List.map
+//             (fun value ->
+//                 match value with
+//                 | Any.Application application ->
+//                     match executeApplication actions bindings application with
+//                     | Ok res -> res
+//                     | Error err -> failwith $"Error: {err.UserMessage}"
+//                 | Any.Record record -> Any.Record(evalRecord actions bindings record)
+//                 | _ -> value)
+//             args)
+// | None, None -> error $"Could not find function {fn}" None
+
+
+
 // match lookupFn actions action with
 // | Some(Fn.Full(_, action)) -> action actions stack
 // | Some(Fn.Stack(_, action)) ->
@@ -110,24 +130,7 @@ and executeExpression (actions: Fns) (bindings: Bindings) (expression: Any) : Re
     | Any.Record record -> Ok(Any.Record(evalRecord actions bindings record))
     | Any.Literal literal -> Ok(Any.Literal literal)
     | Any.Term term -> Ok(Any.Term term)
-    | Any.Application(fn, args) ->
-        match bindings.TryFind fn, actions.TryFind fn with
-        | Some lambda, _ -> evalLambda actions bindings lambda
-        | None, Some(Fn(_, fn)) ->
-            fn
-                actions
-                bindings
-                (List.map
-                    (fun value ->
-                        match value with
-                        | Any.Application application ->
-                            match executeApplication actions bindings application with
-                            | Ok res -> res
-                            | Error err -> failwith $"Error: {err.UserMessage}"
-                        | Any.Record record -> Any.Record(evalRecord actions bindings record)
-                        | _ -> value)
-                    args)
-        | None, None -> error $"Could not find function {fn}" None
+    | Any.Application application -> executeApplication actions bindings application
     | _ -> failwith "TODO"
 // match lookupFn actions action with
 // | Some(Fn.Full(_, action)) -> action actions stack
