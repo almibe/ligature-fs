1fb5621d629c4a25299eff82ae3949c4423a5e73
Alex Michael Berry <alexmiberry@gmail.com>
2025-03-30 13:27:26 -0400

    filling in canvas api

--- src/LigatureFable/Library.fs
+++ src/LigatureFable/Library.fs
@@ -133,10 +133,83 @@ let appendCanvas element (value: Result<Any, LigatureError>) =
         | Some (Any.Tuple instructions) -> 
                 List.iter (fun instruction -> 
                     match instruction with
-                    | Any.Tuple [ Any.Term (Term "fill-style"); Any.Term (Term value) ] ->
-                        emitJsStatement (value) "ctx.fillStyle = $0"
+                    | Any.Tuple [ Any.Term (Term "clear-rect"); Any.Term (Term value) ] ->
+                        emitJsStatement (x, y, w, h) "ctx.clearRect($0, $1, $2, $3)"
                     | Any.Tuple [ Any.Term (Term "fill-rect"); Any.Term (Term x); Any.Term (Term y); Any.Term (Term w); Any.Term (Term h)] ->
                         emitJsStatement (x, y, w, h) "ctx.fillRect($0, $1, $2, $3)"
+                    | Any.Tuple [ Any.Term (Term "stroke-rect"); Any.Term (Term x); Any.Term (Term y); Any.Term (Term w); Any.Term (Term h)] ->
+                        emitJsStatement (x, y, w, h) "ctx.strokeRect($0, $1, $2, $3)"
+
+                    | Any.Tuple [ Any.Term (Term "fill-text"); Any.Literal (Literal text); Any.Term (Term x); Any.Term (Term y)] ->
+                        emitJsStatement (text, x, y) "ctx.fillText($0, $1, $2)"
+                    | Any.Tuple [ Any.Term (Term "stroke-text"); Any.Literal (Literal text); Any.Term (Term x); Any.Term (Term y)] ->
+                        emitJsStatement (text, x, y) "ctx.strokeText($0, $1, $2)"
+
+                    | Any.Tuple [ Any.Term (Term "line-width"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.lineWidth($0)"
+                    | Any.Tuple [ Any.Term (Term "line-cap"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.lineCap($0)"
+                    | Any.Tuple [ Any.Term (Term "line-join"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.lineJoin($0)"
+                    | Any.Tuple [ Any.Term (Term "miter-limit"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.miterLimit($0)"
+                    //TODO line dash
+
+                    | Any.Tuple [ Any.Term (Term "font"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.font($0)"
+                    | Any.Tuple [ Any.Term (Term "font"); Any.Literal (Literal value)] ->
+                        emitJsStatement (value) "ctx.font($0)"
+                    | Any.Tuple [ Any.Term (Term "text-align"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.textAlign($0)"
+                    | Any.Tuple [ Any.Term (Term "text-baseline"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.textBaseline($0)"
+                    | Any.Tuple [ Any.Term (Term "direction"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.direction($0)"
+                    | Any.Tuple [ Any.Term (Term "letter-spacing"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.letterSpacing($0)"
+                    | Any.Tuple [ Any.Term (Term "font-kerning"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.fontKerning($0)"
+                    | Any.Tuple [ Any.Term (Term "font-stretch"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.fontStretch($0)"
+                    | Any.Tuple [ Any.Term (Term "font-variant-caps"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.fontVariantCaps($0)"
+                    | Any.Tuple [ Any.Term (Term "text-rendering"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.textRendering($0)"
+                    | Any.Tuple [ Any.Term (Term "word-spacing"); Any.Term (Term value)] ->
+                        emitJsStatement (value) "ctx.wordSpacing($0)"
+
+                    | Any.Tuple [ Any.Term (Term "fill-style"); Any.Term (Term value) ] ->
+                        emitJsStatement (value) "ctx.fillStyle = $0"
+                    | Any.Tuple [ Any.Term (Term "stroke-style"); Any.Term (Term value) ] ->
+                        emitJsStatement (value) "ctx.strokeStyle = $0"
+
+                    //TODO Gradients and patterns
+                    //TODO Shadows
+
+                    | Any.Tuple [ Any.Term (Term "begin-path")] ->
+                        emitJsStatement () "ctx.beginPath()"
+                    | Any.Tuple [ Any.Term (Term "close-path")] ->
+                        emitJsStatement () "ctx.closePath()"
+                    | Any.Tuple [ Any.Term (Term "move-to"); Any.Term (Term x); Any.Term (Term y)] ->
+                        emitJsStatement (x, y) "ctx.moveTo($0, $1)"
+                    | Any.Tuple [ Any.Term (Term "line-to"); Any.Term (Term x); Any.Term (Term y)] ->
+                        emitJsStatement (x, y) "ctx.lineTo($0, $1)"
+                    | Any.Tuple [ Any.Term (Term "bezier-curve-to"); Any.Term (Term cp1x); Any.Term (Term cp1y); Any.Term (Term cp2x); Any.Term (Term cp2y); Any.Term (Term x); Any.Term (Term y)] ->
+                        emitJsStatement (cp1x, cp1y, cp2x, cp2y, x, y) "ctx.bezierCurveTo($0, $1, $2, $3, $4, $5)"
+                    | Any.Tuple [ Any.Term (Term "quadratic-curve-to"); Any.Term (Term cpx); Any.Term (Term cpy); Any.Term (Term x); Any.Term (Term y)] ->
+                        emitJsStatement (cpx, cpy, x, y) "ctx.bezierCurveTo($0, $1, $2, $3)"
+                    //TODO arc
+                    //TODO arc-to
+                    //TODO ellipse
+                    //TODO rect
+                    //TODO round-rect
+
+                    | Any.Tuple [ Any.Term (Term "fill")] ->
+                        emitJsStatement () "ctx.fill()"
+                    | Any.Tuple [ Any.Term (Term "stroke")] ->
+                        emitJsStatement () "ctx.stroke()"                    
+
+
                     | x -> failwith $"Invalid instruction: {x}.") instructions
         | _ -> failwith "Instructions required when drawing canvas."
         emitJsStatement () "element.appendChild(canvas)"
