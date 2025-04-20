1335e07fd734839a08650ae9bdd3180dfb714153
Alex Michael Berry <alexmiberry@gmail.com>
2025-04-20 16:40:50 -0400

    adding files for pretty printer, #258

---
+++ src/Wander.Test/PrettyPrinter.Test.fs
@@ -0,0 +1,22 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+module Wander.PrettyPrinter.Test
+
+open Expecto
+open Ligature.Model
+open Wander.Model
+
+[<Tests>]
+let tests =
+    testList
+        "Pretty Printer Tests"
+        [ testCase "Print Text" <| fun _ -> 
+            Expect.equal (format (PrintToken.Text "Text")) "Text" ""
+          testCase "Print Break" <| fun _ -> 
+            Expect.equal (format PrintToken.Break) "" ""
+          testCase "Print empty Group" <| fun _ -> 
+            Expect.equal (format (PrintToken.Group [])) "" ""
+          testCase "Print small group" <| fun _ -> 
+            Expect.equal (format (PrintToken.Group [PrintToken.Text "a"; PrintToken.Text "b"])) "a b" ""]
---
+++ src/Wander/PrettyPrinter.fs
@@ -0,0 +1,24 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+module Wander.PrettyPrinter
+
+[<RequireQualifiedAccess>]
+type PrintToken =
+    | Text of string
+    | Break
+    | Nest of int * PrintToken list
+    | Group of PrintToken list
+
+let rec format (token: PrintToken): string =
+    match token with
+    | PrintToken.Text text -> text
+    | PrintToken.Break -> ""
+    | PrintToken.Group group ->
+        List.fold (fun state value -> 
+            if state = "" then
+                format value
+            else
+                state + " " + format value) "" group
+    | PrintToken.Nest (_, _) -> failwith "TODO"
