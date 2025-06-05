61442197c5a0a9589d6eaec5d66801ec6cdd3520
Alex Michael Berry <alexmiberry@gmail.com>
2025-06-05 03:09:45 -0400

    adding LigatureDesktop

---
+++ src/LigatureDesktop/Program.cs
@@ -0,0 +1,31 @@
+﻿using Photino.NET;
+using System.Drawing;
+
+namespace LigatureDesktop
+{
+    //NOTE: To hide the console window, go to the project properties and change the Output Type to Windows Application.
+    // Or edit the .csproj file and change the <OutputType> tag from "WinExe" to "Exe".
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string windowTitle = "Ligature Desktop";
+
+            var window = new PhotinoWindow()
+                .SetTitle(windowTitle)
+                .SetUseOsDefaultSize(false)
+                .SetSize(new Size(1024, 800))
+                .Center()
+                .RegisterWebMessageReceivedHandler((object sender, string message) =>
+                {
+                    var window = (PhotinoWindow)sender;
+                    string response = $"Received message: \"{message}\"";
+                    window.SendWebMessage(response);
+                })
+                .Load(new Uri("localhost:5173"));
+
+            window.WaitForClose();
+        }
+    }
+}
---
+++ src/LigatureDesktop/node-src/app/src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+import '$lib/Files.svelte'
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import '@shoelace-style/shoelace/dist/components/split-panel/split-panel.js'
+//import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
+import { showEditor } from '@ligature/ligature-components'
+
+import '../style.css'
+import Files from '$lib/Files.svelte';
+
+
+
+// Set the base path to the folder you copied Shoelace's assets to
+//setBasePath('/path/to/shoelace/dist');
+
+setTimeout(() => {
+    showEditor(document.querySelector("#editor"), "(docs)")
+})
+
+// <sl-button>, <sl-icon>, <sl-input>, and <sl-rating> are ready to use!
+</script>
+
+<sl-split-panel id="main-split">
+    <div
+        slot="start"
+        style="height: 100%; ">
+        <Files></Files>
+    </div>
+    <div
+        slot="end"
+        style="height: 100%; ">
+        <div id="editor"></div>
+        <div id="results"></div>
+    </div>
+</sl-split-panel>
